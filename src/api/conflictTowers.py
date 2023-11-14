5454bd5c962cabb47736b3d8beb2e96750d707af
damlry <damien.leroy999@gmail.com>
2023-11-14 16:10:34 +0100

    feat: add assets

--- src/api/conflictTowers.py
+++ src/api/conflictTowers.py
@@ -6,6 +6,9 @@ import time
 load_dotenv()
 arbitrerSecret = os.getenv('arbitrerSecret')
 
+COLUMNS = 13
+ROWS = 21
+
 agent = pytactx.Agent(playerId="667VELIB",
 						arena="conflicttower",
 						username="demo",
@@ -22,10 +25,25 @@ def initArbitrers():
 						verbosity=2)
 	
 	time.sleep(1)
+	map = [[0 for j in range(COLUMNS)] for i in range(ROWS)]
 
-	arbitre1.ruleArena("bgImg", "https://github.com/DAMIENLRY/ConflictTower/blob/main/background.png")
+	"""	
+	for i in range(ROWS):
+		for j in range(COLUMNS):
+			if (i==0 or i==ROWS-1) or (j==0 or j==COLUMNS-1):
+				map[i][j] = 1
+	"""
+
+	for i in range(COLUMNS):
+		if i not in (2,3,9,10):
+			map[10][i] = 1 
+
+	arbitre1.ruleArena("bgImg", "https://raw.githubusercontent.com/DAMIENLRY/ConflictTower/main/background.png")
+	arbitre1.ruleArena("gridColumns", COLUMNS)
+	arbitre1.ruleArena("gridRows", ROWS)
+	arbitre1.ruleArena("map", map)
 	arbitre1.ruleArena("mapFriction", [0,1,1,1])
-	arbitre1.ruleArena("mapImgs", ["rgba(255,255,255,1)","rgba(255,255,255,1)","",""])
+	#arbitre1.ruleArena("mapImgs", ["","rgba(255,255,255,1)"])
 	#arbitre1.ruleArena("mapImgs", ["barbare-bg.png","","",""])
 
 	return arbitre1
