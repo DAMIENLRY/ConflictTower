7cfd930bded6d2ead4865a45f948a13b5c902c99
TheHangel <105165180+TheHangel@users.noreply.github.com>
2023-12-05 10:15:56 +0100

    tests cartes (nuls)

---
+++ src/api/tests/battflefield.py
@@ -0,0 +1,65 @@
+import unittest
+import sys
+import os
+current_directory = os.path.dirname(os.path.abspath(__file__))
+parent_directory = os.path.dirname(current_directory)
+grandparent_directory = os.path.dirname(parent_directory)
+sys.path.append(grandparent_directory)
+
+from server.res.BattleField import BattleField
+from server.res.cards.InterfaceCard import InterfaceCard
+from server.res.cards.InterfaceCase import InterfaceCase
+from server.res.cards.EmptyCase import EmptyCase
+from server.res.cards.ObstacleCase import ObstacleCase
+from server.res.cards.states import AttackState
+
+class TestBattleField(unittest.TestCase):
+
+    def setUp(self):
+        self.battlefield = BattleField.getInstance()
+
+    def test_getInstance(self):
+        self.assertEqual(self.battlefield, BattleField.getInstance())
+
+    def test_initMap(self):
+        self.battlefield.initMap()
+        self.assertIsInstance(self.battlefield._map, list)
+        self.assertIsInstance(self.battlefield._map[0][0], EmptyCase)
+
+    def test_getMap(self):
+        self.battlefield.initMap()
+        map = self.battlefield.getMap()
+        self.assertIsInstance(map, list)
+        self.assertIsInstance(map[0][0], int)
+
+    def test_addTroop(self):
+        troop = InterfaceCard()
+        self.battlefield.addTroop(troop)
+        self.assertIn(troop, self.battlefield._troops)
+
+    def test_removeTroop(self):
+        troop = InterfaceCard()
+        self.battlefield.addTroop(troop)
+        self.battlefield.removeTroop(troop)
+        self.assertNotIn(troop, self.battlefield._troops)
+
+    def test_updateMap(self):
+        troop = InterfaceCard()
+        self.battlefield.addTroop(troop)
+        self.battlefield.updateMap()
+        self.assertIsInstance(self.battlefield._map[troop.getX()][troop.getY()], InterfaceCard)
+
+    def test_isOccupiedByOpponent(self):
+        troop = InterfaceCard()
+        self.battlefield.addTroop(troop)
+        self.battlefield.updateMap()
+        self.assertEqual(self.battlefield.isOccupiedByOpponent(troop.getX(), troop.getY()), troop)
+
+    def test_checkAndUpdateCardStates(self):
+        troop = InterfaceCard()
+        self.battlefield.addTroop(troop)
+        self.battlefield.checkAndUpdateCardStates()
+        self.assertIsInstance(troop.state, AttackState)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ src/api/tests/cards.py
@@ -0,0 +1,53 @@
+import unittest
+import sys
+import os
+current_directory = os.path.dirname(os.path.abspath(__file__))
+parent_directory = os.path.dirname(current_directory)
+grandparent_directory = os.path.dirname(parent_directory)
+sys.path.append(grandparent_directory)
+from server.res.cards.InterfaceCard import InterfaceCard
+from server.res.cards.enums.EnumEntitySpeed import EnumEntitySpeed
+from server.res.cards.enums.EnumEntityType import EnumEntityType
+from server.res.cards.enums.EnumSide import EnumSide
+from server.res.cards.states.StateCard import StateCard
+
+class TestInterfaceCard(unittest.TestCase):
+
+    def setUp(self):
+        self.card = InterfaceCard()
+
+    def test_state(self):
+        state = StateCard()
+        self.card.state = state
+        self.assertEqual(self.card.state, state)
+
+    def test_getSide(self):
+        self.card._side = EnumSide.SIDE_1
+        self.assertEqual(self.card.getSide(), EnumSide.SIDE_1)
+
+    def test_isWithinBounds(self):
+        self.assertTrue(self.card.isWithinBounds(5, 5))
+        self.assertFalse(self.card.isWithinBounds(-1, 5))
+        self.assertFalse(self.card.isWithinBounds(5, -1))
+        self.assertFalse(self.card.isWithinBounds(100, 5))
+        self.assertFalse(self.card.isWithinBounds(5, 100))
+
+    def test_getMoveSpeedInterval(self):
+        self.card._SPEED = EnumEntitySpeed.SPEED_1
+        self.assertEqual(self.card.getMoveSpeedInterval(), EnumEntitySpeed.SPEED_1.value)
+
+    def test_setLocation(self):
+        self.assertTrue(self.card.setLocation(5, 5))
+        self.assertFalse(self.card.setLocation(-1, 5))
+        self.assertFalse(self.card.setLocation(5, -1))
+        self.assertFalse(self.card.setLocation(100, 5))
+        self.assertFalse(self.card.setLocation(5, 100))
+
+    def test_opponentInRange(self):
+        self.card._RANGE = 1
+        self.card._x_position = 5
+        self.card._y_position = 5
+        self.assertIsNone(self.card.opponentInRange())
+
+if __name__ == '__main__':
+    unittest.main()
